23b68a1fefe6c981d891158facb2c2823bf5061a
Kathryn Chadwick <zur5ms@virginia.edu>
2025-04-08 22:27:12 -0400

    address eslint issue

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,8 +10,8 @@ export default function AboutPage() {
       <section className="py-20 px-6 max-w-3xl mx-auto text-center">
         <h2 className="text-3xl font-bold mb-6">About Me</h2>
         <p className="text-lg mb-4">
-          I'm Kate, a frontend-focused full stack developer who loves crafting clean, modern, and intuitive UIs.
-          I’m currently in Vanguard's Technology Leadership Program and hold the AWS Cloud Practitioner certification.
+          I&apos;m Kate, a frontend-focused full stack developer who loves crafting clean, modern, and intuitive UIs.
+          I&apos;m currently in Vanguard&apos;s Technology Leadership Program and hold the AWS Cloud Practitioner certification.
         </p>
         <p className="text-lg mb-4">
           Outside of coding, I enjoy fitness, traveling, art, and exploring trendy coffee shops and bakeries.
